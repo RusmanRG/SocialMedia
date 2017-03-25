e266444d881725e5b5e82029c0f3a6bf12f82e32
Russell G Stewart <Russell@Russells-MacBook-Pro.local>
2017-03-25 07:47:22 -0600

    completed firebase display

--- SocialMedia/FeedVC.swift
+++ SocialMedia/FeedVC.swift
@@ -60,7 +60,19 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let post = posts[indexPath.row]
-        print("RUSS: \(post.caption)")
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+        cell.configureCell(post: post)
+            return cell
+        
+        } else {
+            
+            return PostCell()
+        
+        }
+        
+        //let post = posts[indexPath.row]
+        //print("RUSS: \(post.caption)")
         
         
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
--- SocialMedia/PostCell.swift
+++ SocialMedia/PostCell.swift
@@ -14,18 +14,22 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var usernameLbl: UILabel!
     @IBOutlet weak var postImg: UIImageView!
     @IBOutlet weak var caption: UITextView!
-    @IBOutlet weak var likesLbl: UILabel! 
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    var post = Post!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
     }
     
-  
+    func configureCell(post: Post) {
+        self.post = post
+        self.caption.text = post.caption
+        self.likesLbl.text = "\(post.likes)"
+        
+        
     
-    
-    
-    
-    
-    
+    }
+    }
 }
